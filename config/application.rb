708b507da75e28b25229a5131c68eea641398baf
Shubh <shubh.scb@gmail.com>
2022-11-18 00:38:46 +0530

    KEY

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module BudgetNavigant2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.secret_key_base = 'blipblapblup'
 
     # Configuration for the application, engines, and railties goes here.
     #
